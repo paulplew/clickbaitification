7af61c1e581d1f140a566b7d65d52f4b19a1640e
Glitch (glitch-hello-website) <none>
2021-11-09 21:33:20 +0000

    🚘🙏 Checkpoint ./script.js:35990684/14458 undefined:35990684/18 ./articles.json:35990684/5598 ./index.html:35990684/4986 ./style.css:35990684/2201

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
-import articles from 'articles.json'
-const articleContainer = document.querySelector('#content');
+const fetchFile = async filePath => {
+  try {
+    const response = await fetch(filePath);
+    const json = await response.json();
+    return json;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
+const createArticle = jsonArticle => {
+  const image = document.createElement("img");
+  image.src = jsonArticle.url;
+
+  const imageContainer = document.createElement("div");
+  imageContainer.classList.add("image-container");
+  imageContainer.appendChild(image);
+
+  const title = document.createElement("p");
+  title.innerText = jsonArticle.title;
+
+  const titleContainer = document.createElement("div");
+  titleContainer.classList.add("title");
+  titleContainer.appendChild(title);
+
+  const article = document.createElement("a");
+  article.classList.add("article");
+  article.href = jsonArticle.href;
+  article.appendChild(imageContainer);
+  article.appendChild(titleContainer);
+
+  document.querySelector("#content").appendChild(article);
+};
+
+fetchFile("./articles.json").then(articles => {
+  articles.map(element => {
+    createArticle(element);
+  });
+});
